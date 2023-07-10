62e9713856534517ede8e8e04995e85ecc0712df
codebox124 <rajcodes733@gmail.com>
2023-07-10 06:00:42 -0700

    done

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,6 +8,42 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var num1 = 0, num2 = 0, sum = 0;
+
+  final TextEditingController t1 = TextEditingController(text: '0');
+  final TextEditingController t2 = TextEditingController(text: '0');
+  void doaddition() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      sum = num1 + num2;
+    });
+  }
+
+  void dosub() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      sum = num1 - num2;
+    });
+  }
+
+  void domul() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      sum = num1 * num2;
+    });
+  }
+
+  void dodiv() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      sum = num1 ~/ num2;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,17 +51,70 @@ class _HomePageState extends State<HomePage> {
         title: Text("Calculator"),
       ),
       body: Container(
-        child: Column(
-          children: [
-            TextFormField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(hintText: 'Enter a number'),
-            ),
-            TextFormField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(hintText: 'Enter a number'),
-            )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Output : $sum",
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.purple,
+                    fontWeight: FontWeight.bold),
+              ),
+              TextFormField(
+                controller: t1,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(hintText: 'Enter a number'),
+              ),
+              TextFormField(
+                controller: t2,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(hintText: 'Enter a number'),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  new ElevatedButton(
+                    onPressed: () {
+                      doaddition();
+                    },
+                    child: Text("+"),
+                  ),
+                  new ElevatedButton(
+                    onPressed: () {
+                      dosub();
+                    },
+                    child: Text("-"),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  new ElevatedButton(
+                    onPressed: () {
+                      domul();
+                    },
+                    child: Text("*"),
+                  ),
+                  new ElevatedButton(
+                    onPressed: () {
+                      dodiv();
+                    },
+                    child: Text("/"),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
